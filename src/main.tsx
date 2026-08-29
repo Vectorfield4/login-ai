ee8ad075a5c3a8ce4f4605d44762e6ab65e88c09
vectorfield4 <vectorfield4@gmail.com>
2026-08-29 07:06:34 +0000

    fix: load Roboto font with Cyrillic subsets for consistent menu rendering

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import App from "./App";
 
 const queryClient = new QueryClient();
